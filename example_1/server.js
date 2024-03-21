9871ccd4b022f39e74994ce0ab1a6ea5d1512971
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2024-03-21 14:31:33 +0100

    chat example 1 websocket

---
+++ example_1/server.js
@@ -0,0 +1,33 @@
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 8080 });
+
+const clients = [];
+
+server.on("connection", (socket) => {
+  console.log("Client connected");
+
+  clients.push(socket);
+
+  for (const client of clients) {
+    if (client === socket) {
+      client.send("Welcome to Chat!");
+    } else {
+      client.send("New user connected to Chat");
+    }
+  }
+
+  socket.onmessage = (event) => {
+    // console.log( event.data);
+
+    const data = JSON.parse(event.data);
+
+    for (const client of clients) {
+      if (client === socket) {
+        client.send(`You: ${data.message}`);
+      } else {
+        client.send(`${data.name}: ${data.message}`);
+      }
+    }
+  };
+});
